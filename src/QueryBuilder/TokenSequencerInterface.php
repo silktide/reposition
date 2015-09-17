18cc8307d4105fbc5aef328a247889d9ab213fc7
Danny Smart <downsider84@hotmail.com>
2015-09-17 23:54:58 +0100

    add type hinting to the TokenSequencerInterface

--- src/QueryBuilder/TokenSequencerInterface.php
+++ src/QueryBuilder/TokenSequencerInterface.php
@@ -14,42 +14,114 @@ class TokenSequencerInterface
     const SORT_ASC = 1;
     const SORT_DESC = -1;
 
+    /**
+     * @return string
+     */
     public function getType();
 
+    /**
+     * @return bool
+     */
     public function isQuery();
 
+    /**
+     * @return string
+     */
     public function getCollectionName();
 
+    /**
+     * @return array
+     */
     public function getSequence();
 
+    /**
+     * @param string $type
+     *
+     * @return TokenSequencerInterface
+     */
     public function aggregate($type);
 
+    /**
+     * @return TokenSequencerInterface
+     */
     public function where();
 
+    /**
+     * @param array $by
+     * @return TokenSequencerInterface
+     */
     public function sort(array $by);
 
+    /**
+     * @param int $limit
+     * @param int $offset
+     * @return TokenSequencerInterface
+     */
     public function limit($limit, $offset = null);
 
+    /**
+     * @param array $by
+     * @return TokenSequencerInterface
+     */
     public function group(array $by);
 
+    /**
+     * @param mixed $content
+     */
     public function closure($content = null);
 
+    /**
+     * @return TokenSequencerInterface
+     */
     public function notL();
 
+    /**
+     * @return TokenSequencerInterface
+     */
     public function andL();
 
+    /**
+     * @return TokenSequencerInterface
+     */
     public function orL();
 
+    /**
+     * @param string $field
+     * @param string $alias
+     * @param string $type - 'field' or 'table'
+     * @return TokenSequencerInterface
+     */
     public function ref($field, $alias = "", $type = "field");
 
+    /**
+     * @param string $op
+     * @return TokenSequencerInterface
+     */
     public function op($op);
 
+    /**
+     * @param mixed $value
+     * @return TokenSequencerInterface
+     */
     public function val($value);
 
+    /**
+     * @param object|array $entity
+     * @return TokenSequencerInterface
+     */
     public function entity($entity);
 
+    /**
+     * @param string $name
+     * @param array $args
+     * @return TokenSequencerInterface
+     */
     public function func($name, array $args = []);
 
+    /**
+     * @param $keyword
+     * @return TokenSequencerInterface
+     */
     public function keyword($keyword);
 
 } 
