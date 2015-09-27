136069156e0775eb9aa9fd845ecfab1c225d902e
Danny Smart <downsider84@hotmail.com>
2015-09-27 15:26:16 +0100

    implement basic EntityMetadataFactory to use as a default

---
+++ src/Metadata/EntityMetadataFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Silktide\Reposition\Metadata;
+
+class EntityMetadataFactory implements EntityMetadataFactoryInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createMetadata($reference)
+    {
+        if (is_object($reference)) {
+            $reference = get_class($reference);
+        }
+
+        return new EntityMetadata($reference);
+    }
+
+} 
