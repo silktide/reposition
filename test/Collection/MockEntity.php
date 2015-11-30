e6d27d7cdc756196763690e69851350d7915451d
Danny Smart <dannysmart@silktide.com>
2015-11-30 18:30:24 +0000

    Test for the new Collection object

---
+++ test/Collection/MockEntity.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Silktide\Reposition\Test\Collection;
+
+/**
+ * MockEntity
+ */
+class MockEntity
+{
+
+    protected $id;
+
+    public $property;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getProperty()
+    {
+        return $this->property;
+    }
+
+}
